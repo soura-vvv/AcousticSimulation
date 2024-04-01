9dea22f827b18ece9425e46ef0d93ef95160f3b2
soura-vvv <souravpande39@gmail.com>
2024-04-01 17:52:51 -0400

    WOrking

--- zoroRoom.py
+++ zoroRoom.py
@@ -1,20 +1,33 @@
+from email.mime import audio
 import pyroomacoustics as pra
 import numpy as np
+from scipy.io.wavfile import write
+from pyroomacoustics.directivities import (
+    DirectivityPattern,
+    DirectionVector,
+    CardioidFamily,
+)
+dir_obj = CardioidFamily(
+    orientation=DirectionVector(azimuth=90, colatitude=15, degrees=True),
+    pattern_enum=DirectivityPattern.CARDIOID,
+)
+
+
+
 
 # Parameters
 sample_rate = 16000
 
 # Define minimum and maximum values
-min_room_volume = 25  # cubic meters
+min_room_volume = 100  # cubic meters
 max_room_volume = 1000  # cubic meters
-min_rt60 = 0.3  # seconds
+min_rt60 = 5  # seconds
 
 
 max_rt60 = 12  # seconds
-min_sources = 1
+min_sources = 5
 max_sources = 20
-min_source_microphone_distance = 1  # meters
-max_source_microphone_distance = 5  # meters
+
 
 num_microphone_positions_length = 10
 num_microphone_positions_width=5
@@ -35,7 +48,7 @@ print("rt_60valjues")
 print(rt60_values)
 print("source_counts")
 print(source_counts)
-print("source_microphone_distances")
+#print("source_microphone_distances")
 #print(source_microphone_distances)
 
 
@@ -44,8 +57,14 @@ print("source_microphone_distances")
 corpus = pra.datasets.CMUArcticCorpus(download=False)
 # http://www.festvox.org/cmu_arctic/
 
+dataset_num_samples=15583
 print(corpus)
-exit()
+corpus.head(n=10)
+audioz=np.array(corpus[0].data.astype(float))
+print("AUDIO LENGTH")
+print(audioz.size)
+#playsound(corpus[0])
+
 
 # Loop over all combinations
 for room_volume in room_volumes:
@@ -63,14 +82,17 @@ for room_volume in room_volumes:
             # Split the room into two halves
             half_length = room_dimensions[0] / 2
             # Calculate absorption
-            e_absorption, max_order = pra.inverse_sabine(rt60, room_dimensions)
+            #e_absorption, max_order = pra.inverse_sabine(rt60, room_dimensions)
 
             print("Room Dimensions:")
             print(room_dimensions)
             print("rt60")
             print(rt60)
             # Create the room
-            room = pra.ShoeBox(room_dimensions, fs=sample_rate, materials=pra.Material(e_absorption), max_order=max_order)
+            print("ORDERRR")
+            #print(max_order)
+            #exit()
+            room = pra.ShoeBox(room_dimensions, fs=sample_rate)#, materials=pra.Material(e_absorption), max_order=max_order)
             sources=[]
             # Add sources
             for _ in range(num_sources):
@@ -82,18 +104,22 @@ for room_volume in room_volumes:
                 print(source_position)
                 
             for source in (sources):
-                room.add_source(source)
+                room.add_source(source,signal=corpus[np.random.randint(0,dataset_num_samples)].data.astype(float))
+                #room.sources[]
 
             # Add microphone
             microphone_position = [room_dimensions[0], room_dimensions[1]/2, microphone_height]
-            room.add_microphone(microphone_position)
+            #microphone_position = [1.5,1.5,1.7]
+            room.add_microphone(microphone_position,directivity=dir_obj)
             print("Microphone Position")
             print(microphone_position)
             # Compute RIR
-            print("Compute------------------------------------------")
-            room.compute_rir()
-                
-
+            print("Compute UNZOOMED AUDIO-------------------SIMULATE")
+            room.simulate()
+            print(len(room.mic_array.signals[0]))
+            write("exampleRIR.wav", sample_rate, room.rir[0][0].astype(np.int16))
+            write("example.wav", sample_rate, room.mic_array.signals[0].astype(np.int16))
+            exit()
             #Switch Microphone Positions (Moving it closer to the source graudually)
             for i in range(1, num_microphone_positions_length + 1):
                 # Move microphone position
@@ -110,7 +136,7 @@ for room_volume in room_volumes:
                     room = pra.ShoeBox(room_dimensions, fs=sample_rate, materials=pra.Material(e_absorption), max_order=max_order)
                     #Add Sources
                     for source in (sources):
-                        room.add_source(source)
+                        room.add_source(source,signal=corpus[np.random.randint(0,dataset_num_samples)].data.astype(float))
                         print("Source Position:")
                         print(source_position)
                     
