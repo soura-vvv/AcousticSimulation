51c1c03f20d3e24b748d85a4b0a49104035f92e3
soura-vvv <souravpande39@gmail.com>
2024-04-02 17:09:54 -0400

    DataGen Readyyyy

--- TestRoom.py
+++ TestRoom.py
@@ -2,6 +2,7 @@ from email.mime import audio
 from random import sample
 import pyroomacoustics as pra
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 from pyroomacoustics.directivities import (
     DirectivityPattern,
@@ -13,12 +14,18 @@ dir_obj = CardioidFamily(
     pattern_enum=DirectivityPattern.CARDIOID,
 )
 
-room_dimensions=[100,100,100]
+room_dimensions=[10,10,10]
 sample_rate=16000
 
-room = pra.AnechoicRoom(fs=sample_rate)
-room.add_source([10,10,10])
-room.add_microphone([100,100,100])
+room = pra.ShoeBox(room_dimensions, fs=sample_rate,max_order=10)
+room.add_source([1,1,5])
+room.add_microphone([9,9,5])
+room.image_source_model()
+
 room.compute_rir()
-print(len(room.rir[0]))
+print(room.rir[0][0])
+plt.figure(1)
+plt.title("Signal Wave...")
+plt.plot(room.rir[0][0])
+plt.show()
 write("exampleRIR.wav", sample_rate, room.rir[0][0].astype(np.int16))
--- zoroRoom.py
+++ zoroRoom.py
@@ -3,32 +3,35 @@ import pyroomacoustics as pra
 import numpy as np
 from scipy.io.wavfile import write
 import os
-from pyroomacoustics.directivities import (
-    DirectivityPattern,
-    DirectionVector,
-    CardioidFamily,
-)
-dir_obj = CardioidFamily(
-    orientation=DirectionVector(azimuth=90, colatitude=15, degrees=True),
-    pattern_enum=DirectivityPattern.CARDIOID,
-)
+import subprocess
+#import soundfile as sf
+#import pyflac
+import json
+import matplotlib.pyplot as plt
+
+import itertools
+#dir_obj = CardioidFamily(
+#    orientation=DirectionVector(azimuth=90, colatitude=15, degrees=True),
+#    pattern_enum=DirectivityPattern.CARDIOID,
+#)
 
 
 
+# Define a set of typical room sizes
+typical_lengths = [5, 7, 9, 11, 13, 15]  # meters
+typical_widths = [5, 7, 9, 11, 13, 15]    # meters
+typical_heights = [2.5, 3, 3.5, 4, 4.5, 5] # meters
 
+# Generate combinations of room dimensions
+room_dimensions_list = list(itertools.product(typical_lengths, typical_widths, typical_heights))
+
+#for dims in room_dimensions_list:
+    #print("Room Dimensions:", dims)
 # Parameters
 sample_rate = 16000
-
-# Define minimum and maximum values
-min_room_volume = 25  # cubic meters
-max_room_volume = 100  # cubic meters
-min_rt60 = 5  # seconds
-
-
-max_rt60 = 12  # seconds
+file_count=0
 min_sources = 1
-max_sources = 20
-
+max_sources = 10
 
 num_microphone_positions_length = 10
 num_microphone_positions_width=5
@@ -39,49 +42,31 @@ microphone_height = 1.7  # meters
 sources=[]
 # Generate all possible combinations
 
-room_volumes = np.arange(min_room_volume, max_room_volume + 1, 10)
-#rt60_values = np.linspace(min_rt60, max_rt60, num=10)
 source_counts = np.arange(min_sources, max_sources + 1)
-#source_microphone_distances = np.linspace(min_source_microphone_distance, max_source_microphone_distance, num=5)
-print("room_volumes:")
-print(room_volumes)
-#print("rt_60valjues")
-#print(rt60_values)
-print("source_counts")
-print(source_counts)
-#print("source_microphone_distances")
-#print(source_microphone_distances)
-
-
-
 
 corpus = pra.datasets.CMUArcticCorpus(download=False)
 # http://www.festvox.org/cmu_arctic/
 
-dataset_num_samples=15583
-print(corpus)
-corpus.head(n=10)
-audioz=np.array(corpus[0].data.astype(float))
-print("AUDIO LENGTH")
-print(audioz.size)
+dataset_num_samples=len(corpus)
 #playsound(corpus[0])
 
-
+json_file="zoro.json"
 # Loop over all combinations
-for room_volume in room_volumes:
+for room_dimensions in room_dimensions_list:
     for num_sources in source_counts:
         
         print("-----------------------------------------------------------------")
         # Calculate room dimensions
-        min_dimension = max(2.5, (room_volume / 2.5) ** (1 / 3))
-        max_dimension = max(2.5, (room_volume / 2.5) ** (1 / 3))
-        room_dimensions = [np.random.uniform(min_dimension, max_dimension),
-                            np.random.uniform(min_dimension, max_dimension),
-                            np.random.uniform(min_dimension, max_dimension)]
-
+        #min_dimension = max(2.5, (room_volume / 2.5) ** (1 / 3))
+        #max_dimension = max(2.5, (room_volume / 2.5) ** (1 / 3))
+        #room_dimensions = [np.random.uniform(min_dimension, max_dimension),
+        #                    np.random.uniform(min_dimension, max_dimension),
+        #                    np.random.uniform(min_dimension, max_dimension)]
+        #room_dimensions=[18,18,18]
         # Split the room into two halves
-        directory = os.path.join("Dataset", f"{room_dimensions}_{num_sources}","Unzoomed")
-        os.makedirs(directory,exist_ok=True)
+        #room_dimensions=[15,15,5]
+        directoryUnzoomed = os.path.join("Dataset", f"{room_dimensions[0]}_{room_dimensions[1]}_{room_dimensions[2]}_{num_sources}","Unzoomed")
+        os.makedirs(directoryUnzoomed,exist_ok=True)
         half_length = room_dimensions[0] / 2
         
         # Calculate absorption
@@ -89,14 +74,10 @@ for room_volume in room_volumes:
 
         print("Room Dimensions:")
         print(room_dimensions)
-        #print("rt60")
-        #print(rt60)
-        # Create the room
-        print("ORDERRR")
-        #print(max_order)
-        #exit()
+
         room = pra.ShoeBox(room_dimensions, fs=sample_rate)#, materials=pra.Material(e_absorption), max_order=max_order)
         sources=[]
+        signals=[]
         # Add sources
         for _ in range(num_sources):
             source_position = [np.random.uniform(0,half_length),
@@ -104,27 +85,36 @@ for room_volume in room_volumes:
                                 source_height]
             sources.append(np.round(source_position,2))
             print("Source Position:")
-            print(source_position)
+            print(np.round(source_position,2))
             
         for source in (sources):
-            room.add_source(source,signal=corpus[np.random.randint(0,dataset_num_samples)].data.astype(float))
+            sig=corpus[np.random.randint(0,dataset_num_samples)].data.astype(float)
+            room.add_source(source,signal=sig)
+            signals.append(sig)
             #room.sources[]
 
         # Add microphone
         microphone_position = [room_dimensions[0], room_dimensions[1]/2, microphone_height]
         #microphone_position = [1.5,1.5,1.7]
-        room.add_microphone(microphone_position,directivity=dir_obj)
+        room.add_microphone(microphone_position)#,directivity=dir_obj)
         print("Microphone Position")
         print(microphone_position)
         # Compute RIR
         print("Compute UNZOOMED AUDIO-------------------SIMULATE")
         room.simulate()#reference_mic=0,snr=-10)
-        print(len(room.mic_array.signals[0]))
-        #write("exampleRIR.wav", sample_rate, room.rir[0][0].astype(np.int16))
+        #print(len(room.mic_array.signals[0]))
+        write(f"{directoryUnzoomed}/{microphone_position[0]}_{microphone_position[1]}_{microphone_position[2]}.wav", sample_rate, room.mic_array.signals[0].astype(np.int16))
+        file_count+=1
         
-        write(f"{directory}/{microphone_position}.wav", sample_rate, room.mic_array.signals[0].astype(np.int16))
-        directory = os.path.join("Dataset", f"{room_dimensions}_{num_sources}","Zoomed")
-        os.makedirs(directory,exist_ok=True)
+        
+        
+        #plt.title("Signal Wave...")
+        #plt.plot(room.mic_array.signals[0])
+        #plt.show()
+        
+        directoryZoomed = os.path.join("Dataset", f"{room_dimensions[0]}_{room_dimensions[1]}_{room_dimensions[2]}_{num_sources}","Zoomed")
+        os.makedirs(directoryZoomed,exist_ok=True)
+        
         #Switch Microphone Positions (Moving it closer to the source graudually)
         for i in range(1, num_microphone_positions_length + 1):
             # Move microphone position
@@ -140,10 +130,12 @@ for room_volume in room_volumes:
                 # Create the room
                 room = pra.ShoeBox(room_dimensions, fs=sample_rate)
                 #Add Sources
+                idx=0
                 for source in (sources):
-                    room.add_source(source,signal=corpus[np.random.randint(0,dataset_num_samples)].data.astype(float))
+                    room.add_source(source,signal=signals[idx])
                     print("Source Position:")
-                    print(source_position)
+                    print(source)
+                    idx+=1
                 
                 #Add Microphone
                 room.add_microphone(microphone_position)
@@ -153,7 +145,31 @@ for room_volume in room_volumes:
                 #print(microphone_position)
                 room.simulate()
                 print("Compute------------------------------------------")
-                write(f"{directory}/{microphone_position}.wav", sample_rate, room.mic_array.signals[0].astype(np.int16))
+                
+                write(f"{directoryZoomed}/{microphone_position[0]}_{microphone_position[1]}_{microphone_position[2]}.wav", sample_rate, room.mic_array.signals[0].astype(np.int16))
+                file_count+=1
+                
+                coordinatex=np.round((microphone_position[0]-(room_dimensions[0]/2))/(room_dimensions[0]/2),2)
+                coordinatey=np.round(microphone_position[1]/room_dimensions[1],2)
+                length=np.round(len(room.mic_array.signals[0])/sample_rate,2)
+                jsonData={f"{room_dimensions[0]}_{room_dimensions[1]}_{room_dimensions[2]}_{num_sources}/{microphone_position[0]}_{microphone_position[1]}_{microphone_position[2]}":{
+                        "wav":f"{directoryZoomed}/{microphone_position[0]}_{microphone_position[1]}_{microphone_position[2]}.wav",
+                        "length":length,
+                        "coordinates":[coordinatex,coordinatey],
+                        "noisy_wav":f"{directoryUnzoomed}/{microphone_position[0]}_{microphone_position[1]}_{microphone_position[2]}.wav"
+                }}
+                
+                with open(json_file, 'r') as f:
+                    existing_data = json.load(f)
+
+                # Update existing data with new data
+                existing_data.update(jsonData)
+
+                # Write updated data back to JSON file
+                with open(json_file, 'w') as f:
+                    json.dump(existing_data, f, indent=4)
+                    
+                
                 # Compute RIR for the new microphone position
                 #room.compute_rir()
                 #num_microphone_positions_width-=2
@@ -167,11 +183,12 @@ for room_volume in room_volumes:
             # Move microphone closer to the half line
             #microphone_position[0] -= source_microphone_distance
             #room.move_microphone(microphone_position)
-            print(len(room.rir[0]))
             # Plot RIR (optional)
             # room.plot_rir()
             
             # Save RIR or further processing
             # Save or process room impulse response here
-        exit()
+        
 
+print("Total Number of files: ")
+print(file_count)
